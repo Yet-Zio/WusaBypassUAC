facd0d02ef7b1ed5a5a9942e8516dad677e38fc3
Yet-Zio <maheshdeluxe53@gmail.com>
2021-06-30 19:07:02 +0530

    add source

---
+++ comctl32/comctl32.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#pragma comment (linker, "/export:InitCommonControlsEx=c:/windows/system32/comctl32.InitCommonControlsEx,@93")
+#pragma comment (linker, "/export:#344=c:/windows/system32/comctl32.#344,@344,NONAME")
